08070371d7730a49b32ea6fd70fc7729cd76c9aa
sparshdang <dang.sparsh01@gmail.com>
2024-03-02 16:32:13 +0530

    Changed to dedicated gateways

--- src/components/FilesContainer.jsx
+++ src/components/FilesContainer.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 
 import style from "./FilesContainer.module.css";
 
-export default function FilesContainer({data}) {
+export default function FilesContainer({ data }) {
+  const downloadFile = async (hash, name) => {
+    const blob = getBlob(hash);
+    const blobUrl = URL.createObjectURL(blob);
+    
+    const link = document.createElement("a");
+    link.href = blobUrl;
+    link.download = name;
+    link.dispatchEvent(
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+      })
+    );
+
+    URL.revokeObjectURL(blobUrl);
+  };
+
+  const getBlob = async (hash) => {
+    const url = `https://orange-gigantic-wren-85.mypinata.cloud/ipfs/${hash}?pinataGatewayToken=${process.env.REACT_APP_PINATA_GATEWAY_KEY}`;
+    const res = await fetch(url);
+    return await res.blob();
+  }
+
+
   return (
     <div className={style.files__container}>
       <h3>Received files:</h3>
+
       <motion.div
         className={style.files}
         initial={{ opacity: 0 }}
@@ -14,18 +40,13 @@ export default function FilesContainer({data}) {
       >
         {data.map((item, i) => {
           return (
-            <a
-              href={`https://gateway.pinata.cloud/ipfs/${item[1].substring(
-                7
-              )}/`}
+            <div
+              onClick={() => downloadFile(item[1].substring(7), item[0])}
               key={i}
-              target="_blank"
-              rel="noreferrer"
-              download={item[0]}
               className={style.file}
             >
               {item[0]}
-            </a>
+            </div>
           );
         })}
       </motion.div>
